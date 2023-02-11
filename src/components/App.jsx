439a65f09bdfacabd0c42175f6a7eb7f10bcf261
Skynet4241 <skynet4241@gmail.com>
2023-02-11 23:49:29 +0200

    create componentDidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,67 +15,51 @@ export class App extends Component {
     currentSearch: '',
     pageNumber: 1,
     loading: false,
+    errorMessage: '',
   };
 
   onFormSubmit = async e => {
     e.preventDefault();
-    try {
-      const searchQuery = e.target.elements.input.value.trim().toLowerCase();
-      this.setState({ loading: true });
-      if (!searchQuery.trim()) {
-        toast.error('Please enter a non-empty query!', settings);
-        return;
-      }
-      const { hits } = await getImageList(searchQuery, this.state.pageNumber);
-
-      this.setState({
-        images: [...hits],
-        currentSearch: searchQuery,
-        pageNumber: 1,
-        errorMessage: '',
-      });
-    } catch (error) {
-      this.setState({ errorMessage: error.message });
-    } finally {
-      this.setState({ loading: false });
+    const searchQuery = e.target.elements.input.value.trim().toLowerCase();
+    if (!searchQuery.trim()) {
+      toast.error('Please enter a non-empty query!', settings);
+      return;
     }
+    this.setState({
+      images: [],
+      currentSearch: searchQuery,
+      loadButton: null,
+    });
   };
 
   async componentDidUpdate(prevProps, prevState) {
-    const { pageNumber, currentSearch } = this.state;
+    const { images, pageNumber, currentSearch } = this.state;
     const { currentSearch: prevQuery, pageNumber: prevPage } = prevState;
 
     if (prevQuery !== currentSearch || prevPage !== pageNumber) {
       try {
-        const { totalHits } = await getImageList(currentSearch, pageNumber);
+        this.setState({ loading: true });
+        const { hits, totalHits } = await getImageList(
+          currentSearch,
+          pageNumber
+        );
         this.setState({
           loadButton: pageNumber < Math.ceil(totalHits / 12),
+          images: [...images, ...hits],
         });
       } catch (error) {
-        toast.error(
-          `Something is wrong, try to reload page! Error: ${error.message}`,
-          settings
-        );
+        this.setState({ errorMessage: error.message });
+      } finally {
+        this.setState({ loading: false });
       }
     }
   }
 
   onLoadMoreClick = async () => {
-    try {
-      this.setState({ loading: true });
-      const { images, currentSearch, pageNumber } = this.state;
-      const { hits } = await getImageList(currentSearch, pageNumber + 1);
-
-      this.setState({
-        images: [...images, ...hits],
-        pageNumber: pageNumber + 1,
-        errorMessage: '',
-      });
-    } catch (error) {
-      this.setState({ errorMessage: error.message });
-    } finally {
-      this.setState({ loading: false });
-    }
+    const { pageNumber } = this.state;
+    this.setState({
+      pageNumber: pageNumber + 1,
+    });
   };
 
   render() {
