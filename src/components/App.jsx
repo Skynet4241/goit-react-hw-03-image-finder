38d95383d5953da758ad743ba27dc4fe801d502d
Skynet4241 <skynet4241@gmail.com>
2023-02-11 23:30:06 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,24 +20,19 @@ export class App extends Component {
   onFormSubmit = async e => {
     e.preventDefault();
     try {
-      this.setState({ loading: true });
       const searchQuery = e.target.elements.input.value.trim().toLowerCase();
-
+      this.setState({ loading: true });
       if (!searchQuery.trim()) {
         toast.error('Please enter a non-empty query!', settings);
         return;
       }
-      const { hits, totalHits } = await getImageList(
-        searchQuery,
-        this.state.pageNumber
-      );
+      const { hits } = await getImageList(searchQuery, this.state.pageNumber);
 
       this.setState({
         images: [...hits],
         currentSearch: searchQuery,
         pageNumber: 1,
         errorMessage: '',
-        loadButton: this.state.pageNumber < Math.ceil(totalHits / 12),
       });
     } catch (error) {
       this.setState({ errorMessage: error.message });
@@ -46,20 +41,35 @@ export class App extends Component {
     }
   };
 
+  async componentDidUpdate(prevProps, prevState) {
+    const { pageNumber, currentSearch } = this.state;
+    const { currentSearch: prevQuery, pageNumber: prevPage } = prevState;
+
+    if (prevQuery !== currentSearch || prevPage !== pageNumber) {
+      try {
+        const { totalHits } = await getImageList(currentSearch, pageNumber);
+        this.setState({
+          loadButton: pageNumber < Math.ceil(totalHits / 12),
+        });
+      } catch (error) {
+        toast.error(
+          `Something is wrong, try to reload page! Error: ${error.message}`,
+          settings
+        );
+      }
+    }
+  }
+
   onLoadMoreClick = async () => {
     try {
       this.setState({ loading: true });
       const { images, currentSearch, pageNumber } = this.state;
-      const { hits, totalHits } = await getImageList(
-        currentSearch,
-        pageNumber + 1
-      );
+      const { hits } = await getImageList(currentSearch, pageNumber + 1);
 
       this.setState({
         images: [...images, ...hits],
         pageNumber: pageNumber + 1,
         errorMessage: '',
-        loadButton: pageNumber + 1 < Math.ceil(totalHits / 12),
       });
     } catch (error) {
       this.setState({ errorMessage: error.message });
