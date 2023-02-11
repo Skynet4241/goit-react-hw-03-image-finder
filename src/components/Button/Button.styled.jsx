c8671fd880081b446ba5536de2987d1bddb774fb
Skynet4241 <skynet4241@gmail.com>
2023-02-11 20:37:55 +0200

    added margin for gallery list

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const ButtonMore = styled.button`
-  margin: 20px 0 20px;
+  margin: 0 0 20px;
   padding: 8px 16px;
   border-radius: 2px;
   background-color: #3f51b5;
